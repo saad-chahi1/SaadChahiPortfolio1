8da2dbb33e8d339fedae2ce72f108ffd78d3dffe
saad-chahi <s.chahi@kbm-consulting.com>
2025-04-18 10:42:04 +0100

    change color & cv

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Consultant .Net et Angular", "Ingénieur Informatique"],
+        strings: ["Consultant .Net et angular", "Ingénieur informatique", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Consultant .Net et Angular", "Ingénieur Informatique"],
+        strings: ["Consultant .Net et angular", "Ingénieur informatique", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
